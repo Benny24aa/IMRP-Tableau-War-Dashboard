7603075d88d87b4f0fe2db9e9b519f0efe42f6b8
Benny24aa <harleyb101010@gmail.com>
2025-01-18 15:54:31 +0000

    added run script file

---
+++ Create File.R
@@ -0,0 +1,6 @@
+#### Run all these to create big file
+
+source(file.path('Create Datasets.R'))
+source(file.path('Assign Locations.R'))
+source(file.path("Assign Tiers.R"))
+source(file.path("KD Analysis.R"))
