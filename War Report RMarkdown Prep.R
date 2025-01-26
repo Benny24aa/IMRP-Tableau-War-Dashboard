33a52eae7f5f3913203b0f51da01eab7e0a8c368
Benny24aa <harleyb101010@gmail.com>
2025-01-26 22:51:24 +0000

    rmarkdown graphs added

--- War Report RMarkdown Prep.R
+++ War Report RMarkdown Prep.R
@@ -29,3 +29,23 @@ bar_graph_theme <- theme(axis.title = element_text(colour="#3A3776", family = "s
                          axis.line.x = element_line(colour="black"),
                          axis.line.y = element_line(colour="black"),
                          plot.margin = margin(.5,1,.5,.5, "cm"))
+
+#### Comparing factions overall picture
+
+War_Report_Daily_Death_Count_By_Faction_Data <- IMRP_Full_Census_File_Report %>% 
+  select(killedFactionId, date_killed) %>% 
+  group_by(killedFactionId, date_killed) %>% 
+  summarise(count=n(), .groups = 'drop')
+
+War_Report_Daily_Death_Count_By_Faction_Data$killedFactionId <- as.character(War_Report_Daily_Death_Count_By_Faction_Data$killedFactionId)
+
+War_Report_Daily_Death_Count_By_Faction_Data_Graph <- War_Report_Daily_Death_Count_By_Faction_Data %>% 
+  ggplot(aes(x = date_killed, y = count, group = killedFactionId, color= killedFactionId)) +
+  geom_line(linewidth = 1.5) +
+  geom_point(size = 2.5)+
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 11),  # y-axis breaks
+                     limits = c(0, 1500),  # Set y-axis limits
+                     expand = c(0, 0)) +
+  labs(x = "Date",
+       y = "Number of Deaths") +
+  line_graph_theme
